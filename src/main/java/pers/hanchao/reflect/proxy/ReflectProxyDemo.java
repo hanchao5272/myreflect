5b0d6670b75a31283fe21a84041becb28f8d89aa
hanchao5272 <89298141@qq.com>
2018-03-04 15:55:08 +0800

    CSDN-java反射10-代理Proxy-代码优化

--- src/main/java/pers/hanchao/reflect/proxy/ReflectProxyDemo.java
+++ src/main/java/pers/hanchao/reflect/proxy/ReflectProxyDemo.java
@@ -72,13 +72,16 @@ public class ReflectProxyDemo {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             //如果是print方法，则直接打印
             if ("print".equals(method.getName())) {
-                LOGGER.info("调用了MyInvocationHandler...需要格式化打印...");
+                LOGGER.info("调用了MyInvocationHandler.invoke(proxy,method,args)...不需要事务控制...");
+                method.invoke(proxiedObj, args);
             }
             //如果是save方法，则需要进行事务控制
             if ("save".equals(method.getName())) {
-                LOGGER.info("调用了MyInvocationHandler...需要事务控制...");
+                LOGGER.info("调用了MyInvocationHandler.invoke(proxy,method,args)...需要事务控制...");
+                LOGGER.info("~~~~~~~~~~~~~~~~开启事务~~~~~~~~~~~~~~~~");
+                method.invoke(proxiedObj, args);
+                LOGGER.info("~~~~~~~~~~~~~~~~提交或回滚事务（commit/rollback等）~~~~~~~~~~~~~~~~");
             }
-            method.invoke(proxiedObj, args);
             return proxy;
         }
     }
@@ -88,7 +91,7 @@ public class ReflectProxyDemo {
      *
      * @author hanchao 2018/2/28 21:45
      **/
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static void main(String[] args) throws Throwable {
         /**
          * 在Proxy类中，有成员变量：protected InvocationHandler h;
          * InvocationHandler是 代理实例 的调用处理程序
@@ -119,7 +122,11 @@ public class ReflectProxyDemo {
 
         LOGGER.info("=======================获取调用处理器getInvocationHandler=======================");
         //通过Proxy.getInvocationHandler(proxyInstance)获取指定代理的调用处理器
-        LOGGER.info("通过Proxy.getInvocationHandler(proxyInstance)获取指定代理的调用处理器：" + Proxy.getInvocationHandler(proxyInstance) + "\n");
+        InvocationHandler invocationHandler1 = Proxy.getInvocationHandler(proxyInstance);
+        LOGGER.info("通过Proxy.getInvocationHandler(proxyInstance)获取指定代理的调用处理器：" + invocationHandler1);
+        LOGGER.info("通过代理Proxy的InvocationHandler成员变量执行方法：");
+        invocationHandler1.invoke(proxyInstance,MyServiceImpl.class.getDeclaredMethod("save"),null);
+        System.out.println();
 
         LOGGER.info("=======================判断是否代理类isProxyClass=======================");
         //通过Proxy.isProxyClass(clazz)判断是否是代理类
@@ -134,6 +141,6 @@ public class ReflectProxyDemo {
         //通过代理的构造器创建代理实例
         LOGGER.info("通过proxyClass.getConstructor(InvocationHandler.class).newInstance(invocationHandler)创建代理实例");
         MyService proxyInstance1 = (MyService) proxyClass.getConstructor(InvocationHandler.class).newInstance(invocationHandler);
-        proxyInstance1.print();
+        proxyInstance1.save();
     }
 }
